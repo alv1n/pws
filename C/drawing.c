1eb47d0570c0c281f9560bd839e1dd0307984a4e
Alvin <alvinreumer@gmail.com>
2017-03-01 17:27:50 +0100

    clearing screen

---
+++ C/drawing.c
@@ -0,0 +1,17 @@
+/*
+void DrawPixelCol(int x,int y,int col)
+{
+     int *addr= graphicsAddress;
+}
+
+void clearscreen()
+{
+    for(int x= 0;x<1024;x++)
+    {
+        for(int y=0;y<768;y++)
+        {
+            DrawPixel(x,y);
+        }
+    }
+}
+*/
